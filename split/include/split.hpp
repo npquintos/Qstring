145cc24e454f35f50c25bd9d7d954bae8f6fcdfa
Noel Quintos <npquintos@gmail.com>
2023-10-31 20:17:09 -0600

    Add files via upload

---
+++ split/include/split.hpp
@@ -0,0 +1,227 @@
+#include <cstring>
+#include <string>
+// #include <tuple>
+// #include <exception>
+#include <iostream>
+#include <vector>
+#include <string_view>
+#include "include/doctest.h"
+
+#define MAX_JOINED_CHARACTERS 10000
+// #define MAX_JOINED_CHARACTERS 20
+
+class Qstring {
+    // Usage:
+    // auto xsplit = Qstring(line).split(delimeter);
+    // for (auto word : xsplit) {
+    //      std::cout << word << std::endl
+    // }
+    // if 'word' is to be split, 'ptr' below will
+    // hold the starting indexes of the start of
+    // the word. 'word' will then be modified to
+    // be delimited with \0. For example, say you have
+    // this word and you want to split with 'abcd' as
+    // delimiter, similar to word.split('abcd') in python
+    // for splitting -> "abcdFirstabcdSecondabcdThirdabcd"
+    //                   01234567890123456789012345678901
+    //                              1         2         3
+    //                   ^   ^        ^         ^        ^
+    //                   |   |        |         |        |
+    // ptr --------------+---+--------+---------+--------+
+    //                 s+0,0
+    //                     s+4,5
+    // modified word ->"\0bcdFirst\0bcdSecond\0bcdThird\0bcd"
+    std::string_view line_to_process;
+    std::vector<std::string_view> lines_to_process{};
+    std::string_view strip_default {" \t\n\r"};
+    std::string_view &for_stripping {strip_default};
+    std::string_view join_default {""};
+    std::string_view joining_word;
+    char joined_words[MAX_JOINED_CHARACTERS];
+
+    std::vector<std::string_view> &v_split(const std::string_view delim) {
+        int index = 0; // tracks where we are in the line to split
+        split_words.clear();
+        auto pdelim0 = delim.begin();
+        const int delim_length = delim.length();
+        int match = 0;
+        const auto v_word_start = line_to_process.begin();
+        int prev_index = 0;
+        for (auto line_char : line_to_process) {
+            // std::cout << line_char << *(pdelim0+match)<< " ";
+            ++index;
+            if (line_char == *(pdelim0+match)) {
+                ++match;
+                if (match == delim_length) {
+                    split_words.push_back(std::basic_string_view(v_word_start+prev_index, index-prev_index-delim_length));
+                    prev_index = index;
+                    match = 0;
+                }
+            }
+            else {
+                match = 0;
+            }
+        }
+        // std::cout << index << " " << prev_index << " " << delim_length;
+        if (prev_index == index) {
+            split_words.push_back(std::basic_string_view(v_word_start+prev_index, 0));
+        }
+        else {
+            split_words.push_back(std::basic_string_view(v_word_start+prev_index, index-prev_index));
+        }
+        return split_words;
+    };
+
+    bool isToBeTrimmed(const char c) const {
+        for(const char w : for_stripping) {
+            if(c == w)
+                return true;
+        }
+        return false;
+    };
+
+    std::string_view v_strip() const {
+        auto ppre = line_to_process.begin();
+        auto ppost = line_to_process.end();
+        if (ppre == ppost) {
+            return std::string_view(ppre, 0);
+        }
+        // strip first from the front
+        while ( ppre <= ppost && isToBeTrimmed(*ppre)) {
+            ++ppre;
+        }
+        // Beware of situation where everything are to be stripped.
+        // If so, stop!
+        if (ppre == ppost) {
+            --ppre;
+            return std::string_view(ppre, 0);
+        }
+        // Then, strip coming from the end, going forward
+        // Take note that end() is pointing AFTER the last
+        // characted and therefore have to be moved one
+        // character back so that it points to the last
+        // character of the string before parsing backwards.
+        --ppost;
+        while ( ppre < ppost && isToBeTrimmed(*ppost)) {
+            --ppost;
+        }
+        return std::string_view(ppre, ppost-ppre+1);
+    };
+
+    std::string v_join() {
+        auto pChar = joined_words;
+        int count = 0;
+        int processed_elements = 0;
+        const int no_elements = lines_to_process.size();
+        for (const auto word : lines_to_process) {
+            for (const auto c : word) {
+                *pChar++ = c;
+                ++count;
+                if (count >= MAX_JOINED_CHARACTERS) {
+                    break;
+                }
+            }
+            if (count >= MAX_JOINED_CHARACTERS) {
+                return std::string(joined_words, MAX_JOINED_CHARACTERS);
+            }
+            if (++processed_elements != no_elements) {
+                for (const auto c : joining_word) {
+                    *pChar++ = c;
+                    ++count;
+                    if (count >= MAX_JOINED_CHARACTERS) {
+                        break;
+                    }
+                }
+            }
+            if (count >= MAX_JOINED_CHARACTERS) {
+                return std::string(joined_words, MAX_JOINED_CHARACTERS);
+            }
+        }
+        return std::string(joined_words, count);
+    };
+
+    public:
+        Qstring() {};
+        std::vector<std::string_view> split_words;
+
+        Qstring &operator()(const char* line) {
+            line_to_process = std::string_view(line, strlen(line));
+            return *this;
+        };
+
+        Qstring &operator()(std::string_view line) {
+            line_to_process = line;
+            return *this;
+        };
+
+        Qstring &operator()(const std::vector<const char*> &lines) {
+            lines_to_process.clear();
+            for (auto const &line : lines) {
+                lines_to_process.push_back(std::string_view{line});
+            }
+            return *this;
+        };
+
+        Qstring &operator()(const std::vector<std::string_view> &lines) {
+            lines_to_process = lines;
+            return *this;
+        };
+
+        Qstring &operator()(const std::vector<std::string> &lines) {
+            lines_to_process.clear();
+            for (const auto &line : lines) {
+                lines_to_process.push_back(std::string_view{line});
+            }
+            return *this;
+        };
+
+        // ****************************************************************
+        // ****************   split ***************************************
+        // ****************************************************************
+        std::vector<std::string_view> &split(const char *delim=" ") {
+            return v_split(std::basic_string_view(delim, strlen(delim)));
+        };
+
+        std::vector<std::string_view> &split(const std::string_view &delim) {
+            return v_split(delim);
+        };
+
+        // ****************************************************************
+        // ****************   strip ***************************************
+        // ****************************************************************
+
+        std::string_view strip() {
+            for_stripping = strip_default;
+            return v_strip();
+        };
+
+        std::string_view strip(const char *to_be_stripped_out) {
+            for_stripping = std::string_view(to_be_stripped_out, strlen(to_be_stripped_out));
+            return v_strip();
+        };
+
+        std::string_view strip(const std::string_view &to_be_stripped_out) {
+            for_stripping = to_be_stripped_out;
+            return v_strip();
+        };
+
+
+        // ****************************************************************
+        // ****************   join  ***************************************
+        // ****************************************************************
+
+        std::string join() {
+            joining_word = join_default;
+            return v_join();
+        };
+
+        std::string join(const char *to_be_joined) {
+            joining_word = std::string_view(to_be_joined, strlen(to_be_joined));
+            return v_join();
+        };
+
+        std::string join(const std::string_view &to_be_joined) {
+            joining_word = to_be_joined;
+            return v_join();
+        };
+};
---
+++ split/src/test.cpp
@@ -0,0 +1,245 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "include/split.hpp"
+
+TEST_CASE("Test all Qstring utility tools") {
+    auto qs = Qstring();
+    SUBCASE("######## Test the split() utility ########") {
+        SUBCASE("split with char * line_to_split and char * delimiter") {
+            const char *test = "abcdFirstabcdSecondabcdThirdabcd";
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view(""),
+                                                            std::string_view("First"),
+                                                            std::string_view("Second"),
+                                                            std::string_view("Third"),
+                                                            std::string_view("")
+                                                         };
+            auto split_result = qs(test).split("abcd");
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+        
+        SUBCASE("split with std::string line_to_split and std::string_view delimiter") {
+            std::string test{"FirstabcdSecondabcdThird"};
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view("First"),
+                                                            std::string_view("Second"),
+                                                            std::string_view("Third")
+                                                         };
+            std::string_view delimeter{"abcd"};
+            auto split_result = qs(test).split(delimeter);
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+        
+        SUBCASE("split with std::string line_to_split and std::string_view delimiter") {
+            std::string test{"abcdabcdabcdabcd"};
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view(""),
+                                                            std::string_view(""),
+                                                            std::string_view(""),
+                                                            std::string_view(""),
+                                                            std::string_view("")
+                                                         };
+            std::string_view delimeter{"abcd"};
+            auto split_result = qs(test).split(delimeter);
+            CHECK(split_result.size() == correct_answer.size());
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+        
+        SUBCASE("split with std::string line_to_split and no supplied delimeter") {
+            std::string test{"abc def g x yz "};
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view("abc"),
+                                                            std::string_view("def"),
+                                                            std::string_view("g"),
+                                                            std::string_view("x"),
+                                                            std::string_view("yz"),
+                                                            std::string_view("")
+                                                         };
+            auto split_result = qs(test).split();
+            CHECK(split_result.size() == correct_answer.size());
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+        
+        SUBCASE("split with std::string line_to_split and std::string_view delimiter") {
+            std::string test{"abc"};
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view("abc")
+                                                         };
+            std::string_view delimeter{"abcd"};
+            auto split_result = qs(test).split(delimeter);
+            CHECK(split_result.size() == correct_answer.size());
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+        
+        SUBCASE("test that it correctly revert to default afterwards") {
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view("abc"),
+                                                            std::string_view("def"),
+                                                            std::string_view("g"),
+                                                            std::string_view("x"),
+                                                            std::string_view("yz"),
+                                                            std::string_view("")
+                                                         };
+            auto split_result = qs("abc def g x yz ").split();
+            CHECK(split_result.size() == correct_answer.size());
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+        
+        SUBCASE("split with C-string line_to_split and comma delimeter") {
+            std::vector<std::string_view> correct_answer {
+                                                            std::string_view("abc"),
+                                                            std::string_view("def"),
+                                                            std::string_view("g"),
+                                                            std::string_view("x"),
+                                                            std::string_view("yz"),
+                                                            std::string_view("")
+                                                         };
+            auto split_result = qs("abc,def,g,x,yz,").split(",");
+            CHECK(split_result.size() == correct_answer.size());
+            for (long unsigned int i=0; i<correct_answer.size(); ++i) {
+                CHECK(split_result[i] == correct_answer[i]);
+            }
+        }
+    }
+
+
+    SUBCASE("######## Test the strip() utility ########") {
+        SUBCASE("strip words with leading and trailing spaces") {
+            std::vector<std::tuple<std::string, std::string_view>> testValue_correctAnswer {
+                {"hello word", "hello word"},
+                {"  hello word      ", "hello word"},
+                {"\thello   word      \n", "hello   word"},
+                {"\t\t   l          \n \n    ", "l"},
+                {"                  ", ""},
+                {"", ""}
+            };
+            for (const auto& [testValue, correctAnswer] : testValue_correctAnswer) {
+                CHECK(correctAnswer == qs(testValue).strip());
+            }
+        }
+
+        SUBCASE("strip words with leading and trailing specified characters") {
+            std::vector<std::tuple<std::string, std::string_view>> testValue_correctAnswer {
+                {"hello word", "hello word"},
+                {"00hello word######", "hello word"},
+                {"##hello   word########", "hello   word"},
+                {"0####00l000000#############", "l"},
+                {"00000######00#0", ""},
+                {"", ""}
+            };
+            for (const auto& [testValue, correctAnswer] : testValue_correctAnswer) {
+                CHECK(correctAnswer == qs(testValue).strip("0#"));
+            }
+        }
+
+        SUBCASE("Test that it reverts to default without arguments") {
+            std::vector<std::tuple<std::string, std::string_view>> testValue_correctAnswer {
+                {"hello word", "hello word"},
+                {"  hello word      ", "hello word"},
+                {"\thello   word      \n", "hello   word"},
+                {"\t\t   l          \n \n    ", "l"},
+                {"                  ", ""},
+                {"", ""}
+            };
+            for (const auto& [testValue, correctAnswer] : testValue_correctAnswer) {
+                CHECK(correctAnswer == qs(testValue).strip());
+            }
+        }
+
+    }
+
+    SUBCASE("######## Test the join() utility ########") {
+        SUBCASE("Join vector of C-string words with C-string delimiter") {
+            std::tuple<  std::vector<const char *>,   std::string > testValue_correctAnswer {
+                {
+                    "", 
+                    "FIRST", 
+                    "", 
+                    "", 
+                    "SECOND", 
+                    "" 
+                },                  std::string("xyzFIRSTxyzxyzxyzSECONDxyz")
+            };  // using "xyz" as delimiter
+            const auto& [testValue, correctAnswer] = testValue_correctAnswer;
+            CHECK(correctAnswer == qs(testValue).join("xyz"));
+        }
+        SUBCASE("Join vector of C-string words with C-string delimiter") {
+            std::tuple<  std::vector<const char *>,   std::string > testValue_correctAnswer {
+                {
+                    "abc", 
+                    "FIRST", 
+                    "def", 
+                    "SECOND", 
+                },                  std::string("abc~FIRST~def~SECOND")
+            };  // using "xyz" as delimiter
+            const auto& [testValue, correctAnswer] = testValue_correctAnswer;
+            CHECK(correctAnswer == qs(testValue).join("~"));
+        }
+        SUBCASE("Join vector of string words with C-string delimiter") {
+            std::tuple<  std::vector<std::string>,   std::string  > testValue_correctAnswer {
+                {
+                    std::string(""), 
+                    std::string("FIRST"), 
+                    std::string(""), 
+                    std::string(""), 
+                    std::string("SECOND"), 
+                    std::string("") 
+                },                  std::string("xyzFIRSTxyzxyzxyzSECONDxyz")
+            };  // using "xyz" as delimiter
+            const auto& [testValue, correctAnswer] = testValue_correctAnswer;
+            CHECK(correctAnswer == qs(testValue).join("xyz"));
+        }
+
+        SUBCASE("Join vector of string words with string_view delimiter") {
+            std::tuple<  std::vector<std::string>,   std::string> testValue_correctAnswer {
+                {
+                    std::string(""), 
+                    std::string("FIRST"), 
+                    std::string(""), 
+                    std::string(""), 
+                    std::string("SECOND"), 
+                    std::string("") 
+                },                  std::string("xyzFIRSTxyzxyzxyzSECONDxyz")
+            };  // using "xyz" as delimiter
+            const auto& [testValue, correctAnswer] = testValue_correctAnswer;
+            CHECK(correctAnswer == qs(testValue).join("xyz"));
+        }
+
+        SUBCASE("Join vector of string words with default delimiter") {
+            std::tuple<  std::vector<std::string>,   std::string > testValue_correctAnswer {
+                {
+                    std::string(""), 
+                    std::string("FIRST"), 
+                    std::string(""), 
+                    std::string(""), 
+                    std::string("SECOND"), 
+                    std::string("") 
+                },                  std::string("FIRSTSECOND")
+            };  // No delimeter argument is supplied; same as concatenate
+            const auto& [testValue, correctAnswer] = testValue_correctAnswer;
+            CHECK(correctAnswer == qs(testValue).join());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
